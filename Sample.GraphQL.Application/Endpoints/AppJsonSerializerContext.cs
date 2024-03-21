a7010ce0d36caafdf8446b6a005c7b9c544860cb
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-03-21 21:04:59 +0100

    Adjust solutions

---
+++ Sample.GraphQL.Application/Endpoints/AppJsonSerializerContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EventSource.Application.Account
+{
+    //https://devblogs.microsoft.com/dotnet/system-text-json-in-dotnet-8/
+
+    [JsonSourceGenerationOptions(
+    JsonSerializerDefaults.Web,
+    AllowTrailingCommas = true,
+    UseStringEnumConverter = true,
+    DefaultBufferSize = 10)]
+    [JsonSerializable(typeof(CreateOrderRequest))]
+    public partial class AppJsonSerializerContext : JsonSerializerContext
+    {
+
+    }
+
+}
---
+++ Sample.GraphQL.Application/Endpoints/CinemaEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Sample.GraphQL.Application.Endpoints;
+
+public static class CinemaEndpoint
+{
+    public static void AddEnpoints(this IEndpointRouteBuilder app)
+    {
+
+        app.MapGet("/Test", async () =>
+        {
+            throw new NotImplementedException();
+        });
+
+
+    }
+
+}
